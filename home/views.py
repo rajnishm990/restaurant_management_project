ebf81fce5ebe177f16c5626722e9ef763c7c1c86
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-04 13:19:28 +0530

    19403

--- home/views.py
+++ home/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 from products.model import Item
 
 # Create your views here.
+
+def HomePage(request):
+    products = Item.objects.all()
+    context = {"products":products}
+    return render (request , "index.html", context)
+
+
