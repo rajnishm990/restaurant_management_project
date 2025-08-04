df3ab7d91d597747adf1b3af50ab52c9213d9af3
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-04 12:58:10 +0530

    19402

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    ''' Custom profile model , extends User model  '''
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email =  models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.name if name else self.email
+
+    
