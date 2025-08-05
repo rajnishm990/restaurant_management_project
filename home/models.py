7a3767c403606f78c6b9fec696b302fd94b19319
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-05 11:58:22 +0530

    21802

--- home/models.py
+++ home/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class MenuItem
+
 class Contact(models.Model):
     name = models.CharField(max_length = 100)
     email =  models.EmailField()
