24d2bb37a9dc695865fe43e28c58f4be91206f94
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-05 11:29:32 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+  ''' Custom model or storing info of users Who contact us '''
+  name = models.CharField(max_length =100)
+  email =  models.EmailField()
+
