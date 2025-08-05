0a3428128724d58a35d88a65799901d113e7e945
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-05 11:32:51 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from products.model import Item
+from .forms import ContactForm
 
 # Create your views here.
 
 def HomePage(request):
+    success = False
+    if request.method=='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            success = True
+            form = ContactForm()
+    else:
+        form = ContactForm()
     products = Item.objects.all()
-    context = {"products":products}
+    context = {"products":products , 'form':'form', 'success':success}
     return render (request , "index.html", context)
+
 
 
