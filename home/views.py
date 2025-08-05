614c415065bbaeec969b985afa9eb865453ddde1
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-05 11:43:45 +0530

    21800

--- home/views.py
+++ home/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from products.model import Item
+from products.model import Item 
+from .models import Contact
 
 # Create your views here.
 
 def HomePage(request):
+
+    sucess = False
+    if request.method =='POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+
+        if name and email:
+            Contact.objects.create(name=name, email=email)
+    
     products = Item.objects.all()
-    context = {"products":products}
+    context = {"products":products, 'success':success}
     return render (request , "index.html", context)
 
 
