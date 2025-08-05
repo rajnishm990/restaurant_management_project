bc09c9db0aea95c3c8d3e63adcac2555b9fc4860
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-05 11:44:39 +0530

    21800

--- home/models.py
+++ home/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-  ''' Custom model or storing info of users Who contact us '''
-  name = models.CharField(max_length =100)
-  email =  models.EmailField()
+    name = models.CharField(max_length = 100)
+    email =  models.EmailField()
+
+    def __str__(self):
+        return f"query by {self.name}"
 
