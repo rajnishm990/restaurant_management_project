6cde2986b6ea0db9aa52408d379f481acd1a1ace
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-05 12:07:20 +0530

    21802

--- home/models.py
+++ home/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class MenuItem
+class MenuItem(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    price = models.DecimalField(max_digits = 10 , decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - \n {self.description[:50]}"
+        
 
 class Contact(models.Model):
     name = models.CharField(max_length = 100)
@@ -10,4 +17,6 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"query by {self.name}"
+
+
 
