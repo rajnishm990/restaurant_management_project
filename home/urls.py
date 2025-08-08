aff49ed43b4c95aa2542bb7960553a831904d6c2
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-08 13:08:56 +0530

    26812

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("/", home , name='home'),
-    path( "/about-us", about_us , name='about')
+    path("/", HomePage , name='home'),
+    path( "/about-us", about_us , name='about'),
 ]
