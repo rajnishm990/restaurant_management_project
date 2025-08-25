1efc86a44a502108560ec5219ccc22f4b9f95512
Rajnish Mishra <71627345+rajnishm990@users.noreply.github.com>
2025-08-25 11:50:22 +0530

    36943

--- home/models.py
+++ home/models.py
@@ -6,6 +6,7 @@ class Restaurant(models.Model):
     name =  models.CharField(max_length = 255)
     description = models.TextField()
     #image = models.ImageField(upload_to = )    ## Will do it later
+    phone = models.CharField(max_length=15 )
 
     def __str__(self):
         return self.name
